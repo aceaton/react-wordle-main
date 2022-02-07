188861660d020df06b1b41d4d4dab81a348f4967
aceaton <aceaton@princeton.edu>
2022-02-07 01:45:38 -0500

    added files

---
+++ src/components/modals/AboutModal.tsx
@@ -0,0 +1,22 @@
+import { BaseModal } from './BaseModal'
+
+type Props = {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const AboutModal = ({ isOpen, handleClose }: Props) => {
+  return (
+    <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        I fucked your mom on a boat -{' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a>{' '}
+      </p>
+    </BaseModal>
+  )
+}
